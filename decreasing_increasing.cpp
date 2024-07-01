86969b1ce52df802bf6f96f8649a77b31e69d0c0
Anmolkumar2002 <anmolkumar200210@gmail.com>
2024-07-01 11:22:24 +0530

    initial commit

--- decreasing_increasing.cpp
+++ decreasing_increasing.cpp
@@ -1,12 +1,17 @@
 #include<iostream>
 using namespace std;
-void pid(int n){
-    
+void pdi(int n){
+    if(n == 0){
+        return;
+    }
+    cout << n << endl;
+    pdi(n - 1);
+    cout << n << endl;
 }
 int main(){
     int n;
     cout << "Enter a number:" << endl;
     cin >> n;
-    pid(n);
+    pdi(n);
     return 0;
 }
